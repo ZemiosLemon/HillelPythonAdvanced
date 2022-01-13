14f15507d330140aa7864b5e9ee81a2b1558dfba
andrey <khizhnyak.andrej@gmail.com>
2022-01-13 23:44:12 +0200

    done

---
+++ leson_02/app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from task_1 import student_param
+from task_2 import return_requirements
+from task_3 import random_list
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return
+
+
+@app.route('/get_avr_data')
+def get_avr_data():
+    return f'Средний рост студентов в см: {student_param()[0]} <br> Средний вес студентов в кг: {student_param()[1]}'
+
+
+@app.route('/requirements')
+def get_requirements():
+    return return_requirements().replace('\n', '<br>')
+
+
+@app.route('/random_students')
+def random_students():
+    return '<br>'.join(random_list())
+
+
+
+app.run(debug=True)
+
+
+
