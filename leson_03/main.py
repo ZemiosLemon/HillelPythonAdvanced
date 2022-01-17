1d030c5a9d43cb4e7d67af17b61e579568406879
andrey <khizhnyak.andrej@gmail.com>
2022-01-17 02:45:26 +0200

    done

--- leson_03/main.py
+++ leson_03/main.py
@@ -1,8 +1,25 @@
 from flask import Flask
+from webargs.flaskparser import use_kwargs
+from webargs import fields, validate
+from utils import get_requests
 
 app = Flask(__name__)
 
 
-@app.route("/avr_data")
-def get_avr_data():
-    return
+@app.route('/')
+@use_kwargs(
+    {
+        'currency': fields.Str(
+            required=False,
+            missing='USD',
+            validate=[validate.Length(equal=3)]
+        )
+    },
+    location='query'
+)
+def get_avr_data(currency):
+    result = get_requests(currency)
+    return f'Rate of bitcoin to the {result[0]} is equal to  {result[1]}'
+
+
+app.run(debug=True)
--- leson_03/utils.py
+++ leson_03/utils.py
@@ -1,6 +1,9 @@
 import requests
 
-response = requests.get('https://bitpay.com/api/rates').json()
 
-print(response)
-
+def get_requests(currency):
+    currency = currency.upper()
+    date = requests.get('https://bitpay.com/api/rates').json()
+    for num in date:
+        if num['code'] == currency:
+            return num['name'], num['rate']
