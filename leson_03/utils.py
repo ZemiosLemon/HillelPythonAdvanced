b1217c259ec6f6640f20f29e02f243f4bb3dc66b
andrey <khizhnyak.andrej@gmail.com>
2022-01-18 02:44:55 +0200

    done

--- leson_03/utils.py
+++ leson_03/utils.py
@@ -4,6 +4,7 @@ import requests
 def get_requests(currency):
     currency = currency.upper()
     date = requests.get('https://bitpay.com/api/rates').json()
+
     for num in date:
         if num['code'] == currency:
             return num['name'], num['rate']
