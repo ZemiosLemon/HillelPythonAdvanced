799a7b89e0f7bdb08d6c96a05262eef831c16612
andrey <khizhnyak.andrej@gmail.com>
2022-01-23 21:37:59 +0200

    done

--- leson_07/app.py
+++ leson_07/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from webargs import fields
 from webargs.flaskparser import use_kwargs
-from  utils import top_tracks, list_rec2html
-
+from utils import top_tracks, list_rec2html
 
 app = Flask(__name__)
 
@@ -18,8 +17,6 @@ app = Flask(__name__)
     },
     location='query'
 )
-
-
 def get_top_tracks(count):
     return list_rec2html(top_tracks(count))
 
