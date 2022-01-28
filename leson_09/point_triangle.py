49290244a9c43bc3fa2482271d9ce0c9072109b9
andrey <khizhnyak.andrej@gmail.com>
2022-01-28 21:19:05 +0200

    done

--- leson_09/point_triangle.py
+++ leson_09/point_triangle.py
@@ -3,15 +3,12 @@ class Point:
         self.x = x
         self.y = y
 
-
     def __add__(self, other):
         result = Point(
             self.x + other.x,
             self.y + other.y
         )
         return result
-
-
 
     def __sub__(self, other):
         result = Point(
@@ -20,9 +17,8 @@ class Point:
         )
         return result
 
-
     def __str__(self):
-        return str(self.x)+' '+str(self.y)
+        return str(self.x) + ' ' + str(self.y)
 
 
 class Triangle:
@@ -31,19 +27,17 @@ class Triangle:
         self.b = Point(x2, y2)
         self.c = Point(x3, y3)
 
-
     def s_triangle(self):
-        return 0.5*abs((self.b.x-self.a.x)*(self.c.y-self.a.y)-(self.c.x-self.a.x)*(self.b.y-self.a.y))
+        return 0.5 * abs((self.b.x - self.a.x) * (self.c.y - self.a.y) - (self.c.x - self.a.x) * (self.b.y - self.a.y))
 
-# 1/2*abs((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1))
+    # 1/2*abs((x2-x1)*(y3-y1)-(x3-x1)*(y2-y1))
 
     def p_triangle(self):
         ab = ((self.b.x - self.a.x) ** 2 + (self.b.y - self.a.y) ** 2) ** 0.5
         bc = ((self.c.x - self.b.x) ** 2 + (self.c.y - self.b.y) ** 2) ** 0.5
         ac = ((self.c.x - self.a.x) ** 2 + (self.c.y - self.a.y) ** 2) ** 0.5
-        result  = ab + bc + ac
+        result = ab + bc + ac
         return result
-
 
     def change_coordinates(self, a1, b1, a2, b2, c1, c2):
         self.a.x = a1
@@ -53,9 +47,10 @@ class Triangle:
         self.c.x = c1
         self.c.y = c2
 
-triangele = Triangle(5, 4, -1, 2, -7, 3)
-print(triangele.s_triangle())
-print(triangele.p_triangle())
-triangele.change_coordinates(4, 3, 0, 1, -6, 2)
-print(triangele.p_triangle())
-print(triangele.s_triangle())
+
+triangle = Triangle(5, 4, -1, 2, -7, 3)
+print(triangle.s_triangle())
+print(triangle.p_triangle())
+triangle.change_coordinates(4, 3, 0, 1, -6, 2)
+print(triangle.p_triangle())
+print(triangle.s_triangle())
