12344c48cb61d1c8b609f456ecfd42c7b77a16a3
andrey <khizhnyak.andrej@gmail.com>
2022-02-02 16:06:20 +0200

    done

--- lesson_10/app.py
+++ lesson_10/app.py
@@ -13,25 +13,18 @@ class Circle(Shape):
         super().__init__(x, y)
         self.radius = radius
 
-
     def contains(self, point):
-        result = (self.x - point.x)**2 + (self.y - point.y)
-        if result <= self.radius**2:
+        result = (self.x - point.x) ** 2 + (self.y - point.y)
+        if result <= self.radius ** 2:
             return True
         else:
             return False
 
-
     def __contains__(self, item):
         return self.contains(item)
+
 
 p = Point(4, -4)
 c = Circle(2, 5, 3)
 print(c.contains(p))
 print(p in c)
-
-
-
-
-
-    # def contains(self,Points):
